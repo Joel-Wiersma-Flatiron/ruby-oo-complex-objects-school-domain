9309d10fb1fea04006ef070ff276f3fc813d5453
Joel-Wiersma-Flatiron <Joel.Wiersma.D@gmail.com>
2020-11-04 16:16:08 -0700

    commit tree 5132f9d7eb217b5ddc76427636958d0f4f2dbd4f

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+
+    attr_reader :name,:roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    def add_student(name, grade)
+        if @roster[grade] == nil
+            @roster[grade] = []
+        end
+        @roster[grade] << name
+    end
+    def grade(num)
+        @roster[num]
+    end
+    def sort
+        @roster.each do |grade, name|
+            @roster[grade] = name.sort
+        end
+        @roster
+    end
+end
